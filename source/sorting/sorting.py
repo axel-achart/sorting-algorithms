a1052e568f711cc414176bf9413548da2ed9adca
dina-yahiaoui <dina.yahiaoui@laplateforme.com>
2025-04-08 11:09:50 +0200

    comb_sort

--- source/sorting/sorting.py
+++ source/sorting/sorting.py
@@ -2,9 +2,11 @@
 
 from source.algos.selection_sort import selection_sort
 from source.algos.bubble_sort import bubble_sort
+from source.algos.comb_sort import comb_sort
 
 # Dictionnaire des algorithmes disponibles
 sorting_algorithms = {
     "selection": selection_sort,
     "bubble": bubble_sort,
+    "comb": comb_sort,
 }
